0f6a13f6c3c8cc5e70c20d64950a515229dba83d
Jared Desai <jjd2037@vt.edu>
2022-12-06 00:26:33 -0500

    use range instead of enumerate when actual value isn't needed

--- day6.py
+++ day6.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     with open("day6.txt") as f:
         data = f.read()
     sol1 = sol2 = 0
-    for i, char in enumerate(data):
+    for i in range(len(data)):
         if not sol1 and len(set(data[i:i+4])) == 4: # only want to check if we haven't found the solution yet
             sol1 = i+4
         if sol1 and len(set(data[i:i+14])) == 14: #only check if we have found solution 1
